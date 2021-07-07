13b6d63985875ac473720ee8262a11834f0b0377
bullzeye777 <jesus.aranda7@yahoo.com>
2021-07-07 16:47:52 -0500

    notes commit

--- main.js
+++ main.js
@@ -53,7 +53,43 @@ console.log(`The array in alphabetical order ${carsInReverse}`);
 // Console.log the reptiles array and run the program.
 // Now console.log the pets array and run the program. Why do you think snake and lizard are still in the original array?
 let pets = ['dog', 'cat', 'fish', 'rabbit', 'snake', 'lizard', 'bird'];
-let reptiles = pets.slice(5, 6);
+let reptiles = pets.slice(5, 7);
 console.log(`Array with reptiles ${reptiles}`);
 //The slice() method returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included) where start and end represent the index of items in that array. The original array will not be modified.
 console.log(`The pets array ${pets}`);
+
+// .splice()
+// Create a new array called removedReptiles, using the splice method to remove snake and lizard from the pets array.
+// Console.log removedReptiles and pets and run the program.
+// Go back and add the string 'hamster' in as a third parameter to your splice method, then run the program again and notice how the pets array has changed. Do you see how that works?
+
+// .pop()
+// Use the pop method to remove the last item from the pets array, saving it to a variable called removedPet.
+// Console.log removedPet and pets and run the program.
+
+//.push()
+//Use the push method to add removedPet back to the end of the pets array.
+//Console.log pets and run the program.
+
+//.shift()
+//Use the shift method to remove and console.log the first item in the pets array.
+
+//.unshift()
+//Use the unshift method to add the string 'turtle' as the first item in the pets array.
+//Console.log the pets array and run the program. If all went according to plan, you should see ['turtle', 'cat', 'fish', 'rabbit', 'hamster', 'bird'].
+
+//.forEach()
+//Create a numbers array by copy/pasting the following: const numbers = [23, 45, 0 , 2, 8, 44, 100, 1, 3, 91, 34]
+//Write code that will add 2 to each item in the array numbers.
+//forEach requires a function to be passed into it as its first argument.
+//Build a function called addTwo that can take in num, index, and arr as parameters—(num, index, arr)—and returns num + 2 at each index of the arr.
+//Use .forEach() on the numbers array, passing in your freshly built function addTwo as an argument, in order to add 2 to each number in the array.
+//Console.log numbers and run the program.
+
+/*----------------------------- 
+Hints
+Use repl.it to write the solution code first. (It's a faster environment vs using the node main.js command over and over again.)
+Use your documentation.
+Push yourself to learn on your own. Ask: How does this work?
+Clone, setup, testing, and running instructions for all projects is below
+-------------------------------*/
