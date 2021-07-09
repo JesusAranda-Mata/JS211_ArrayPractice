faa7887ff6153dcdf6e386b99e0d3bcd274aa86e
bullzeye777 <jesus.aranda7@yahoo.com>
2021-07-09 16:15:01 -0500

    unable to figure out forEach will come back later

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ console.log(`The array in alphabetical order ${carsInReverse}`);
 // Console.log the reptiles array and run the program.
 // Now console.log the pets array and run the program. Why do you think snake and lizard are still in the original array?
 let pets = ['dog', 'cat', 'fish', 'rabbit', 'snake', 'lizard', 'bird'];
-let reptiles = pets.slice(5, 7);
+let reptiles = pets.slice(4, 6);
 console.log(`Array with reptiles ${reptiles}`);
 //The slice() method returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included) where start and end represent the index of items in that array. The original array will not be modified.
 console.log(`The pets array ${pets}`);
@@ -62,21 +62,35 @@ console.log(`The pets array ${pets}`);
 // Create a new array called removedReptiles, using the splice method to remove snake and lizard from the pets array.
 // Console.log removedReptiles and pets and run the program.
 // Go back and add the string 'hamster' in as a third parameter to your splice method, then run the program again and notice how the pets array has changed. Do you see how that works?
+let removedReptiles = pets.splice(4, 2, "hamster");
+console.log(`Reptiles are not pets ${removedReptiles}`);
+//hamster replace the reptiles.
+console.log(`The good pets are ${pets}`);
 
 // .pop()
 // Use the pop method to remove the last item from the pets array, saving it to a variable called removedPet.
 // Console.log removedPet and pets and run the program.
+let removedPet = pets.pop()
+console.log(`Removing the last pet ${removedPet}`);
+console.log(`The pets left over ${pets}`);
 
 //.push()
 //Use the push method to add removedPet back to the end of the pets array.
 //Console.log pets and run the program.
+pets.push(removedPet);
+console.log(`The pets array restore ${pets}`);
 
 //.shift()
 //Use the shift method to remove and console.log the first item in the pets array.
+pets.shift()
+console.log(`Removing the first pet ${pets}`);
 
 //.unshift()
 //Use the unshift method to add the string 'turtle' as the first item in the pets array.
-//Console.log the pets array and run the program. If all went according to plan, you should see ['turtle', 'cat', 'fish', 'rabbit', 'hamster', 'bird'].
+//Console.log the pets array and run the program. 
+//If all went according to plan, you should see ['turtle', 'cat', 'fish', 'rabbit', 'hamster', 'bird'].
+pets.unshift("turtle");
+console.log(`The final array ${pets}`);
 
 //.forEach()
 //Create a numbers array by copy/pasting the following: const numbers = [23, 45, 0 , 2, 8, 44, 100, 1, 3, 91, 34]
@@ -85,6 +99,20 @@ console.log(`The pets array ${pets}`);
 //Build a function called addTwo that can take in num, index, and arr as parameters—(num, index, arr)—and returns num + 2 at each index of the arr.
 //Use .forEach() on the numbers array, passing in your freshly built function addTwo as an argument, in order to add 2 to each number in the array.
 //Console.log numbers and run the program.
+const numbers = [23, 45, 0 , 2, 8, 44, 100, 1, 3, 91, 34];
+   //--numbers.forEach(element => console.log(element+2))
+//numbers.forEach(addTwo, index, numbers) 
+
+const addTwo = () => {
+      for (let n = 0; n < numbers.length; n++){
+         let num = numbers[n] + 2;
+         //let num = num+2
+         console.log("results: " + num);
+      }
+   }
+   
+addTwo()
+
 
 /*----------------------------- 
 Hints
